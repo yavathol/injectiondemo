d5ea51599f2e81cc03e811aeba53c1fff30fea8c
Jurek Dörfler <yavathol@gmail.com>
2017-04-12 22:11:18 +0200

    Initial commit

---
+++ src/main/java/pl/ycs/junk/InjectiondemoApplication.java
@@ -0,0 +1,12 @@
+package pl.ycs.junk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InjectiondemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InjectiondemoApplication.class, args);
+	}
+}
